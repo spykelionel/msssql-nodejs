bcb92b95af6639e05c3dd6b049467c2279b85f84
Spyke Lionel <social.lionel@gmail.com>
2024-08-02 16:36:59 +0100

    suscessfully connected to mssql server

---
+++ index.js
@@ -0,0 +1,31 @@
+const sql = require("mssql/msnodesqlv8");
+
+let config = {
+  server: "LIONEL-DELL-WIN",
+  database: "testdb",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true, // Use this for Windows Authentication
+  },
+};
+
+sql.connect(config, function (err) {
+  if (err) {
+    return console.error("Connection error:", err);
+  }
+
+  console.log("Connected to SQL Server...");
+
+  const request = new sql.Request();
+
+  request.query("SELECT * FROM books", function (err, recordset) {
+    if (err) {
+      console.error("Query error:", err);
+      sql.close();
+      return;
+    }
+
+    console.dir(recordset);
+    sql.close();
+  });
+});
